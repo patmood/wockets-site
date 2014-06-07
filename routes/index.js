82bdc02ed7f2646e92c2cf7af2d049df4460f861
patmood <patrick.n.moody@gmail.com>
2014-06-07 11:47:48 -0700

    twilio

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express')
-var router = express.Router()
+  , router = express.Router()
+
+var accountSid = process.env.TWILIO_SID
+  , authToken = process.env.TWILIO_TOKEN
+  , twilio = require('twilio')(accountSid, authToken)
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' })
+})
+
+router.post('/sms', function(req, res) {
+  twilio.sms.messages.create({
+    body: 'Download Wobble Wockets for iOS: http://wobblewockets.com'
+  , to: '+13127140629'
+  , from: '+13314818220'
+  }, function(err, message) {
+    if (err) return res.send('Error:' + err)
+    console.log('SMS sent!')
+  })
+  res.send("done!")
 })
 
 module.exports = router
